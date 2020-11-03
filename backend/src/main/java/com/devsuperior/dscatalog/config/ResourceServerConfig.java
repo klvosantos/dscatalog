5979a3a6309b8ba7ffa009ae8a8c0866f815c76f
klvo <klvo.gtfo.gmail.com>
2020-11-02 21:45:12 -0300

    H2 security configuration

--- backend/src/main/java/com/devsuperior/dscatalog/config/ResourceServerConfig.java
+++ backend/src/main/java/com/devsuperior/dscatalog/config/ResourceServerConfig.java
@@ -1,7 +1,10 @@
 package com.devsuperior.dscatalog.config;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
@@ -13,15 +16,17 @@ import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 @EnableResourceServer
 public class ResourceServerConfig extends ResourceServerConfigurerAdapter{
 	
-	private static final String[] PUBLIC = { "/oauth/token" };
+	@Autowired
+	private Environment env; // Ambiente de execução da aplicação apartir deste objeto é possivel acessar varias variaveis 
+	
+	private static final String[] PUBLIC = { "/oauth/token", "/h2-console/**" };
 	
 	private static final String[] OPERATOR_OR_ADMIN = { "/products/**", "/categories/**" };
 	
 	private static final String[] ADMIN = { "/users/**" };
 	
 	@Autowired
-	private JwtTokenStore tokenStore;
-	
+	private JwtTokenStore tokenStore;	
 	
 	@Override
 	public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
@@ -30,6 +35,12 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter{
 
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
+		
+		// Libera o h2
+		if (Arrays.asList(env.getActiveProfiles()).contains("test")) {
+			http.headers().frameOptions().disable();
+		}
+		
 		
 		http.authorizeRequests()
 		.antMatchers(PUBLIC).permitAll()
