fd755df00cbe2ac5cc2182eb7a362dc3221bc8d8
klvo <klvo.gtfo.gmail.com>
2020-11-04 14:59:05 -0300

    Criado modulo auth - Adicionado biblioteca React Hoor Form para gereciar formularios

--- front-web/src/pages/Auth/components/Login/index.tsx
+++ front-web/src/pages/Auth/components/Login/index.tsx
@@ -1,28 +1,44 @@
 import ButtonIcon from 'core/components/ButtonIcon';
-import React from 'react';
 import { Link } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
+import React from 'react';
 import AuthCard from '../Card';
 import './styles.scss';
 
+type FormData = {    // tipo de dados que serao enviados para a api
+    email: string;
+    password: string;
+}
+
 const Login = () => {
+   const { register, handleSubmit} = useForm<FormData>(); // tipo o useForm com o FormData(fazendo isso o react hook form integra com o typescript) e quando o formulario for submetido ele seguira o modelo de dados estabelecido no FormData
+   
+   const onSubmit = (data: FormData) => {
+        console.log(data);
+   }    
+
    return (
     <AuthCard title="login">
-       <form className="login-form">
+       <form className="login-form" onSubmit={handleSubmit(onSubmit)}>
            <input 
              type="email"
              className="form-control input-base margin-bottom-30"
-             placeholder="Email"    
+             placeholder="Email"
+             name="email"
+             ref={register}    
            />      
            <input 
              type="password"
              className="form-control input-base"
-             placeholder="Senha"    
+             placeholder="Senha" 
+             name="password"
+             ref={register}       
            />
            <Link to="/admin/auth/recover" className="login-link-recover">
               Esqueci a senha?
            </Link>
            <div className="login-submit">
-              <ButtonIcon text="Logar" />      
+               <ButtonIcon text="Logar" /> {/*Como o botao esta dentro do formulario, quando for acionado ele submetera o formulario, e uma vez feito isso o react-hook-form (handleSubmit) ira capturar e tratar essa submissão*/}
            </div>
            <div className="text-center">
               <span className="not-registered">
