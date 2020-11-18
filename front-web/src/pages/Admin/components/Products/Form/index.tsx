834adf1a6d9b3ac9b96327d21ab6d77fdb27e00f
klvo <klvo.gtfo.gmail.com>
2020-11-18 11:26:46 -0300

    Adicionando React Hook form no cadastro de produtos

--- front-web/src/pages/Admin/components/Products/Form/index.tsx
+++ front-web/src/pages/Admin/components/Products/Form/index.tsx
@@ -8,11 +8,10 @@ type FormState = {
     name: string;
     price: string;
     description:string;
-    ImageUrl: string;
+    imageUrl: string;
 }
 
 const Form = () => {
-    const {} = useForm<FormState>();
     const { register, handleSubmit } = useForm<FormState>();
 
     const onSubmit = (data: FormState ) => {  
@@ -20,46 +19,45 @@ const Form = () => {
     }
 
     return (
-        <form onSubmit={handleSubmit(onSubmit)}>
-            <BaseForm title="cadastrar um produto">
-
-                <div className="row">
-                    <div className="col-6">
-                        <input
-                            ref={register({ required: "Campo obrigatório" })}  
-                            name="name"
-                            type="text"
-                            className="form-control margin-bottom-30 input-base"
-                            placeholder="Nome do produto"
-                        />                        
-                        <input
-                            ref={register({ required: "Campo obrigatório" })}  
-                            name="price"
-                            type="number"
-                            className="form-control margin-bottom-30 input-base"
-                            placeholder="Preço"
-                        />
-                        <input
-                            ref={register({ required: "Campo obrigatório" })}  
-                            name="imageUrl"
-                            type="text"
-                            className="form-control margin-bottom-30 input-base"
-                            placeholder="Imagem do produto"
-                        />    
-                    </div>
-                    <div className="col-6">
-                        <textarea
-                            ref={register({ required: "Campo obrigatório" })}  
-                            name="description"
-                            className="form-control input-base"
-                            placeholder="Descrição"
-                            cols={30}
-                            rows={10}
-                        />
-                    </div>
+    <form onSubmit={handleSubmit(onSubmit)}>
+        <BaseForm title="cadastrar um produto">
+            <div className="row">
+                <div className="col-6">
+                    <input
+                        ref={register({ required: "Campo obrigatório" })}  
+                        name="name"
+                        type="text"
+                        className="form-control margin-bottom-30 input-base"
+                        placeholder="Nome do produto"
+                    />                        
+                    <input
+                        ref={register({ required: "Campo obrigatório" })}  
+                        name="price"
+                        type="number"
+                        className="form-control margin-bottom-30 input-base"
+                        placeholder="Preço"
+                    />
+                    <input
+                        ref={register({ required: "Campo obrigatório" })}  
+                        name="imageUrl"
+                        type="text"
+                        className="form-control margin-bottom-30 input-base"
+                        placeholder="Imagem do produto"
+                    />    
                 </div>
-            </BaseForm>
-        </form>
+                <div className="col-6">
+                    <textarea
+                        ref={register({ required: "Campo obrigatório" })}  
+                        name="description"
+                        className="form-control input-base"
+                        placeholder="Descrição"
+                        cols={30}
+                        rows={10}
+                    />
+                </div>
+            </div>
+        </BaseForm>
+    </form>
     )
 }
 
