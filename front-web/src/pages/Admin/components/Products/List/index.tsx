26026aff9416e9165fd064588dec000e423e690b
klvo <klvo.gtfo.gmail.com>
2020-11-24 13:11:41 -0300

    Criando exclusão do produto

--- front-web/src/pages/Admin/components/Products/List/index.tsx
+++ front-web/src/pages/Admin/components/Products/List/index.tsx
@@ -1,35 +1,54 @@
-import Pagination from 'core/components/Pagination';
+import React, { useEffect, useState, useCallback } from 'react';
 import { ProductsResponse } from 'core/types/Product';
-import { makerequest } from 'core/Utils/request';
-import React, { useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom';
-import Card from '../Card';
+import { makePrivateRequest, makerequest } from 'core/Utils/request';
+import { toast } from 'react-toastify';
 import CardLoader from '../Loaders/ProductCardLoader';
+import Pagination from 'core/components/Pagination';
+import Card from '../Card';
 
 const List = () => {
   const [productsResponse, setProductResponse] = useState<ProductsResponse>();
   const [isLoading, setIsLoading] = useState(false);
   const [activePage, setActivePage] = useState(0);
   const history = useHistory();
+
+  const getProducts = useCallback(() => { // useCallback(usa tecnica de memorization) guarda em memoria os valorores da função getProducts, invocano o getProducts apenas se as dependencias sofrerem alterações em suas dependencias, no caso a activePage, useCallback evita de o componente ficar criando referencias para a função getProducts a tdo momento que o componente é rerenderizado.
+    const params = {
+      page: activePage,
+      linesPerPage: 4,
+      direction: 'DESC',
+      orderBy: 'id'
+   }
+    setIsLoading(true)
+      makerequest({ url: '/products', params })
+      .then(response => setProductResponse(response.data))
+      .finally(() => {
+        setIsLoading(false);
+      })
+  }, [activePage]);
   
   useEffect(() => {
-     const params = {
-        page: activePage,
-        linesPerPage: 4,
-        direction: 'DESC',
-        orderBy: 'id'
-     }
-     
-     setIsLoading(true)
-     makerequest({ url: '/products', params })
-     .then(response => setProductResponse(response.data))
-     .finally(() => {
-     setIsLoading(false);
-     })
-  }, [activePage]);
+    getProducts();
+  }, [getProducts]);
 
     const handleCreate = () => {
         history.push('/admin/products/create');
+    }
+
+    const onRemove = (productId: number) => {
+      const confirm = window.confirm('Deseja realmente excluir este produto?');
+
+      if (confirm) {
+          makePrivateRequest({ url: `/products/${productId}`, method: 'DELETE' })  
+          .then(() => {
+            toast.info('Produto removido com sucesso!');
+            getProducts();
+        })
+          .catch(() => {
+            toast.error('Erro ao remover produto!');
+        })
+      }
     }
 
     return (
@@ -40,7 +59,7 @@ const List = () => {
           <div className="admin-list-container">
             {isLoading ? <CardLoader /> : (
               productsResponse?.content.map(product => (
-                <Card product={product} key={product.id} />
+                <Card product={product} key={product.id} onRemove={onRemove}/>
               ))
             )}
             {productsResponse && (
