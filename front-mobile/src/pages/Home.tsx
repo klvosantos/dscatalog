21d7149809da7cdbdf24a097f867d4367c30ec4b
klvo <klvo.gtfo.gmail.com>
2021-01-29 14:42:47 -0300

    Projeto Mobile criado, add rotas basicas

---
+++ front-mobile/src/pages/Home.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { View, Text, TouchableOpacity } from 'react-native';
+
+const Home: React.FC = ({ navigation }) => {
+    return (
+        <View>
+            <Text>Bem vindo ao APP</Text>
+
+            <TouchableOpacity
+                style={{
+                    width: 150,
+                    backgroundColor: "#069",
+                    padding: 10,
+                    borderRadius: 4,
+                }}
+            
+                onPress={() => navigation.navigate('Catalog')}
+            >
+
+            <Text>Clique aqui</Text>    
+
+            </TouchableOpacity>
+        </View>
+    )
+}
+
+export default Home;
---
+++ front-mobile/src/routes/index.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const Stack = createStackNavigator();
+
+import { Home, Catalog } from '../pages'
+
+const Routes: React.FC = () => {
+    return (
+      <Stack.Navigator>  
+        <Stack.Screen 
+        
+        options={{
+          headerShown: true,
+        }}
+
+        name="Home" 
+        component={Home} />
+        <Stack.Screen name="Catalog"component={Catalog} />
+      </Stack.Navigator> 
+    )
+}
+
+export default Routes;
