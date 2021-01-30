1cb9491710037370bb56ae40fc1c457fbd09443e
klvo <klvo.gtfo.gmail.com>
2021-01-30 17:46:50 -0300

    Criado tela Home

--- front-mobile/src/pages/Home.tsx
+++ front-mobile/src/pages/Home.tsx
@@ -1,27 +1,36 @@
 import React from 'react';
-import { View, Text, TouchableOpacity } from 'react-native';
+import { View, Text, TouchableOpacity, Image } from 'react-native';
 
-const Home: React.FC = ({ navigation }) => {
+import { text, theme } from "../styles";
+import { useNavigation } from "@react-navigation/native";
+import arrow from '../assets/arrow.png';
+import draw from '../assets/draw.png';
+
+const Home: React.FC = () => {
+    const navigation = useNavigation();
+
     return (
-        <View>
-            <Text>Bem vindo ao APP</Text>
+        <View style={theme.container}>
+            <View style={theme.card}>
+                <Image source={draw} style={theme.draw}/>
+                <View style={theme.textContainer}>
+                    <Text style={text.bold}>Conheça o melhor catálogo de produtos</Text>
+                    <Text style={text.regular}>Ajudaremos você a encontrar os{"\n"} melhores produtos disponíveis{"\n"} no mercado.</Text>
+                </View>
+                <TouchableOpacity 
+                    style={theme.primaryButton}
+                    activeOpacity={0.8}
+                    onPress={() => navigation.navigate("Catalog")}
 
-            <TouchableOpacity
-                style={{
-                    width: 150,
-                    backgroundColor: "#069",
-                    padding: 10,
-                    borderRadius: 4,
-                }}
-            
-                onPress={() => navigation.navigate('Catalog')}
-            >
-
-            <Text>Clique aqui</Text>    
-
-            </TouchableOpacity>
+                >
+                    <Text style={text.primaryText}>INICIE AGORA A SUA BUSCA</Text>
+                    <View style={theme.arrowContainer}>
+                        <Image source={arrow}/>
+                    </View>
+                </TouchableOpacity>
+            </View>
         </View>
     )
 }
 
-export default Home;
+export default Home;  
--- front-mobile/src/routes/index.tsx
+++ front-mobile/src/routes/index.tsx
@@ -8,14 +8,7 @@ import { Home, Catalog } from '../pages'
 const Routes: React.FC = () => {
     return (
       <Stack.Navigator>  
-        <Stack.Screen 
-        
-        options={{
-          headerShown: true,
-        }}
-
-        name="Home" 
-        component={Home} />
+        <Stack.Screen name="Home" component={Home} />
         <Stack.Screen name="Catalog"component={Catalog} />
       </Stack.Navigator> 
     )
