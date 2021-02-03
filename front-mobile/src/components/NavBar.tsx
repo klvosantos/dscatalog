fd14e7c27b3f5a781be6d3c8e4ddfeecd198518b
klvo <klvo.gtfo@gmail.com>
2021-02-03 15:11:05 -0300

    Finalizando barra de navegação

---
+++ front-mobile/src/components/NavBar.tsx
@@ -0,0 +1,71 @@
+import React, { useEffect, useState } from "react";
+import { useNavigation, useRoute } from "@react-navigation/native";
+import { TouchableOpacity, Image, View, Text } from "react-native";
+import { nav } from "../styles";
+import { TouchableNativeFeedback } from "react-native-gesture-handler";
+
+import menu from "../assets/menu.png";
+const NavBar: React.FC = () => {
+    const [ show, setShow ] = useState(false);
+    const navigation = useNavigation();
+    const route = useRoute();
+    
+
+    function navigate(path: any) {
+        if (path) {
+            setShow(false);
+            navigation.navigate(path); // esse navigate não tem ligação com o nome da função navigate.
+        }
+        setShow(false);
+    }
+    
+
+    return (
+        <TouchableOpacity 
+            activeOpacity={0.8}
+            style={nav.drawer}
+            onPress={() => setShow(!show)}
+        >
+            <Image source={menu} />
+            {show ? (
+                <View style={nav.options}>
+                    <TouchableNativeFeedback 
+                    style={nav.option}
+                    onPress={() => navigate("Home")}
+                    >
+                        <Text 
+                        style={[
+                            nav.textOption,
+                            route.name === "Home" ? nav.textActive : null
+                            ]}
+                        >
+                            Home
+                        </Text>
+                    </TouchableNativeFeedback>
+
+                    <TouchableNativeFeedback 
+                    style={nav.option} 
+                    onPress={() => navigate("Catalog")}>
+                        <Text style={[nav.textOption,
+                            route.name === "Catalog" ? nav.textActive : null
+                            ]}
+                        >
+                            Catalogo
+                        </Text>
+                    </TouchableNativeFeedback>
+
+                    <TouchableNativeFeedback style={nav.option}>
+                        <Text style={[nav.textOption,
+                            route.name === "ADM" ? nav.textActive : null
+                            ]}
+                            >
+                            ADM
+                        </Text>
+                    </TouchableNativeFeedback>                    
+                </View>
+            ) : null}
+        </TouchableOpacity>
+    );
+};
+
+export default NavBar;
--- front-mobile/src/routes/index.tsx
+++ front-mobile/src/routes/index.tsx
@@ -1,13 +1,26 @@
 import React from 'react';
+import { Text } from 'react-native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { Home, Catalog, ProductDetails } from '../pages'
+import { NavBar } from "../components";
+import { colors, nav } from '../styles';
+
+
+const HeaderText: React.FC = () => <Text style={nav.leftText}>DS Catalog</Text>;
 
 const Stack = createStackNavigator();
-
-import { Home, Catalog, ProductDetails } from '../pages'
-
 const Routes: React.FC = () => {
     return (
-      <Stack.Navigator>  
+      <Stack.Navigator
+        screenOptions={{
+          headerTitle: "",
+          headerStyle: {
+            backgroundColor: colors.primary          
+          },
+          headerLeft: () => <HeaderText />,
+          headerRight: () => <NavBar />
+        }}
+      >  
         <Stack.Screen name="Home" component={Home} />
         <Stack.Screen name="Catalog"component={Catalog} />
         <Stack.Screen name="ProductDetails" component={ProductDetails} />
